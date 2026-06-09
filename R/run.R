59eb2e6924ecda0634e49ca62acc27ea3bcbedde
Ben Brumpton <42624014+benbrumpton@users.noreply.github.com>
2026-06-09 09:02:36 +0200

    Create run.R

---
+++ R/run.R
@@ -0,0 +1,6 @@
+setwd("/Users/benb/Downloads/sem-vs-regression/")
+
+source("R/01_generate_data.R")
+source("R/02_regression_model.R")
+source("R/03_covariance_sem.R")
+source("R/04_compare_results.R")
